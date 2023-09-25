945c7fdb825ddacd0909bd6b2672fe1d54405b36
frhl <frederik.heymann@gmail.com>
2023-09-25 15:51:54 +0100

    FIX: Now merging by phenotype instead of chroms

--- 04_merge.sh
+++ 04_merge.sh
@@ -11,9 +11,6 @@ set -o errexit
 set -o nounset
 
 pheno_dir="/well/lindgren-ukbb/projects/ukbb-11867/flassen/projects/KO/wes_ko_ukbb_nexus/data/phenotypes"
-
-
-
 gene_map="/mnt/project/genesets/220524_hgnc_ensg_enst_chr_pos.txt.gz"
 in_dir="/mnt/project/wes_ko_ukbb/data/saige/step2/binary/by_chrom"
 out_dir="/wes_ko_ukbb/data/saige/step2/binary"
@@ -22,24 +19,25 @@ dx mkdir -p ${out_dir}
 
 for anc in eur; do
    bin_phenos="${pheno_dir}/bin_matrix_eur_header.txt"
-   for pheno in $(cat $bin_phenos | head -n1); do
+   for pheno in $(cat $bin_phenos | head -n100); do
       for anno in "pLoF_damaging_missense"; do
-         for mode in "recessive"; do
+         for mode in "additive"; do
             for af in "01"; do
                for pp in "0.90"; do
-                 chr_type=$(get_chr_type ${chr})
-                 pattern="UKB.${chr}.${mode}.${anc}.af${af}.pp${pp}.${anno}"
-                 out_prefix="UKB.merged.${mode}.${anc}.af${af}.pp${pp}.${anno}"
+                 pattern="UKB.STAR.${pheno}.${mode}.${anc}.af${af}.pp${pp}.${anno}.txt.gz"
+                 out_prefix="UKB.${pheno}.${mode}.${anc}.af${af}.pp${pp}.${anno}"
+                 info_names="annotation|mode|af_cutoff|pp_cutoff|ancestry"
                  info="${anno}|${mode}|${af}|${pp}|${anc}"
                  dx run app-swiss-army-knife \
                   -iimage_file="/docker/rsuite.tar.gz"\
                   -icmd="
                    Rscript /mnt/project/${rscript_remote} \
-                     --input_dir ${in_dir} \
-                     --pattern ${pattern} \
-                     --gene_map_path ${gene_map} \
-                     --column_info ${column_new} \
-                     --out_prefix ${out_prefix}
+                     --out_prefix '${out_prefix}' \
+                     --input_dir '${in_dir}' \
+                     --pattern '${pattern}' \
+                     --gene_map_path '${gene_map}' \
+                     --column_info_names '${info_names}' \
+                     --column_info '${info}'
                  "\
                 --instance-type mem1_ssd1_v2_x4 \
                 --folder=".${out_dir}" \
